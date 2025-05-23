9ec45e571afadbaf4e8feaa7b6ac56ed716f31e9
limou3434 <898738804@qq.com>
2025-05-23 10:50:37 +0800

    尝试封装 RPC 全局异常处理器成功

--- work-user-service/src/main/java/cn/com/edtechhub/workuserserver/service/UserServiceImpl.java
+++ work-user-service/src/main/java/cn/com/edtechhub/workuserserver/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package cn.com.edtechhub.workuserserver.service;
 
+import cn.com.edtechhub.workgeneralmodule.exception.CodeBindMessageEnums;
+import cn.com.edtechhub.workgeneralmodule.utils.ThrowUtils;
 import cn.com.edtechhub.workuserserver.api.UserService;
 import org.apache.dubbo.config.annotation.DubboService;
 
@@ -8,6 +10,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String test() {
+        ThrowUtils.throwIf(true, CodeBindMessageEnums.SUCCESS, "true");
         return "Hello work-user-server!";
     }
 
